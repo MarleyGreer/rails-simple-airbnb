e50eaacb01bf466a7ca7f33004376f0dc71da39a
Marley Greer <marley.greer93@gmail.com>
2020-08-27 17:56:49 +1000

    basic site complete, added crud options on flats

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.6'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
@@ -16,7 +17,12 @@ gem 'webpacker', '~> 4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'autoprefixer-rails'
+gem 'faker'
+gem 'font-awesome-sass', '~> 5.6.1'
 gem 'jbuilder', '~> 2.7'
+gem 'simple_form'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Flat.create(
+  name: Faker::Address.street_name,
+  address: Faker::Address.full_address,
+  description: Faker::Marketing.buzzwords,
+  price_per_night: (50..700).to_a.sample,
+  number_of_guests: (2..12).to_a.sample,
+  image_url: ["https://images.unsplash.com/photo-1564013799919-ab600027ffc6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    "https://images.unsplash.com/photo-1587240853651-4b7deabd5449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80 750w",
+    "https://images.unsplash.com/photo-1573663520878-8c38b10264fc?ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80 967w"].sample
+ )
+end
